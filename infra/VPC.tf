2ebfa5f68ee926ea193405b5d87adf9d4720e5be
luankenzley <kenzleydev@outlook.com>
2024-05-11 15:27:38 -0300

    ajustes

--- infra/VPC.tf
+++ infra/VPC.tf
@@ -6,21 +6,8 @@ module "vpc" {
 
   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = [
-    {
-      cidr_block = "10.0.101.0/24"
-      map_public_ip_on_launch = true
-    },
-    {
-      cidr_block = "10.0.102.0/24"
-      map_public_ip_on_launch = true
-    },
-    {
-      cidr_block = "10.0.103.0/24"
-      map_public_ip_on_launch = true
-    }
-  ]
-  database_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  database_subnets= ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
   create_database_subnet_group           = true
   create_database_subnet_route_table     = true
@@ -30,4 +17,6 @@ module "vpc" {
   enable_dns_support   = true
 
   enable_nat_gateway = true
+
+  map_public_ip_on_launch = true
 }
